44dcf45ec73d4a6998b7c5b2e1c0b0cee41faff3
BestAlice <dima_goloskok@lenta.ru>
2020-10-10 17:47:35 +0300

    прозрачыне изображения

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ MOVEMENT_SPEED = 5
 
 class Player(arcade.Sprite):
     def __init__(self):
-        super().__init__("knight.jpg", SPRITE_SCALING)
+        super().__init__("hero.png", SPRITE_SCALING)
         
     def update(self):
         self.center_x += self.change_x
@@ -32,7 +32,7 @@ class Player(arcade.Sprite):
 
 class Guard(arcade.Sprite):
     def __init__(self):
-        super().__init__("guard.jpg", SPRITE_SCALING)
+        super().__init__("guard.png", SPRITE_SCALING)
         self.center_x = SCREEN_WIDTH / 2
         self.center_y = SCREEN_HEIGHT / 2
     
